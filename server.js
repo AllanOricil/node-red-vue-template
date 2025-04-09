a554cb093c5e023d224a156cfcf4b85e93a34e7f
Allan Oricil <allanoricilcos@outlook.com>
2025-04-09 11:14:35 -0300

    chore: remove dist files from repo

--- server.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (RED) {
-  function RemoteServerNode(config) {
-    RED.nodes.createNode(this, config);
-    this.name = config.name;
-    this.host = config.host;
-  }
-  RED.nodes.registerType("remote-server", RemoteServerNode);
-};
