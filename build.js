3ec78ae6ab19e32894e81c4ae2e228354b06f1ac
Allan Oricil <allanoricilcos@outlook.com>
2025-05-09 19:06:24 -0300

    chore: cleanup dependencies and esbuild script

--- build.js
+++ build.js
@@ -1,23 +1,22 @@
 const esbuild = require("esbuild");
 const esbuildPluginTsc = require("esbuild-plugin-tsc");
-
-const common = {
-  bundle: true,
-  platform: "node",
-  target: "node18",
-  format: "cjs",
-  plugins: [esbuildPluginTsc()],
-  minify: false,
-  keepNames: true,
-  external: ["*.node", "aws-cdk-lib", "constructs", "@aws-cdk"],
-  loader: { ".js": "jsx" },
-};
+const { dtsPlugin } = require("esbuild-plugin-d.ts");
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
 
 esbuild
   .build({
     entryPoints: ["src/server.ts"],
     outfile: "index.js",
-    treeShaking: false,
-    ...common,
+    sourcemap: true,
+    bundle: true,
+    platform: "node",
+    target: "node18",
+    format: "cjs",
+    plugins: [nodeExternalsPlugin(), esbuildPluginTsc(), dtsPlugin()],
+    minify: false,
+    keepNames: true,
+    treeShaking: true,
+    external: ["*.node"],
+    loader: { ".js": "jsx" },
   })
   .catch(() => process.exit(1));
