81ab52dcbc47dd80f9ae2ff0dc5a7b812f16ed3c
Allan Oricil <allanoricilcos@outlook.com>
2025-05-16 18:32:17 -0300

    chore: fix small typo

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,7 +5,7 @@ import { registerType } from "../../core/client";
 // TODO: is there a use case for controlling the order nodes in the editor are registered?
 async function main() {
   try {
-    console.log("Registering node types in parellel");
+    console.log("Registering node types in parallel");
     await Promise.all([
       registerType("remote-server", remoteServer),
       registerType("your-node", yourNode),
